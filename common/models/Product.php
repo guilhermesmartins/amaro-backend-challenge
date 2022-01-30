c218d73d6640036ee1a306af95efe5e86181ba3d
guilhermeSmartins <mineg4500@gmail.com>
2022-01-30 03:33:55 -0300

    feat(Product): creation of product model
    Creation of Product Model from Gii

---
+++ common/models/Product.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace common\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "product".
+ *
+ * @property int $id
+ * @property string|null $name
+ * @property int|null $product_id
+ */
+class Product extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'product';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['product_id'], 'integer'],
+            [['name'], 'string', 'max' => 120],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'name' => 'Name',
+            'product_id' => 'Product ID',
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     * @return ProductQuery the active query used by this AR class.
+     */
+    public static function find()
+    {
+        return new ProductQuery(get_called_class());
+    }
+}
