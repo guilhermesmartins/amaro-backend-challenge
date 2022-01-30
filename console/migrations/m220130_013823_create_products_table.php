bdbebb2ff0f840806c74606358bda4ba7cd216d6
guilhermeSmartins <mineg4500@gmail.com>
2022-01-29 22:42:11 -0300

    feat(create_products_table): create products table migration
    BREAKING CHANGE: affects db

---
+++ console/migrations/m220130_013823_create_products_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%products}}`.
+ */
+class m220130_013823_create_products_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%products}}', [
+            'id' => $this->primaryKey(),
+            'name' => $this->string(120),
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%products}}');
+    }
+}
